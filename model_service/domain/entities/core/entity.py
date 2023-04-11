b4a5293c016759895ce22d517eaf536f52e17562
Ivan Krasnoshchok <ivankras67@gmail.com>
2023-04-11 12:41:54 +0300

    Add entities.core
    Add BaseEntity class with .create method
    Add @entity decorator

---
+++ model_service/domain/entities/core/entity.py
@@ -0,0 +1,25 @@
+from typing import Type, ClassVar, Dict, TypeVar
+
+import attrs
+
+from model_service.domain.entities.core.generated import GeneratedType, GENERATED_VALUE, is_generated
+
+
+TEntity = TypeVar("TEntity", bound="BaseEntity")
+
+
+def entity(cls: Type[TEntity]) -> Type[TEntity]:
+    cls._generated_attributes = {attr.name: GENERATED_VALUE for attr in attrs.fields(cls) if is_generated(attr.type)}
+    return cls
+
+
+@entity
+@attrs.define
+class BaseEntity:
+    _generated_attributes: ClassVar[Dict[str, GeneratedType]]
+
+    @classmethod
+    def create(cls: Type[TEntity], **kwargs) -> TEntity:
+        generated_attributes = cls._generated_attributes.copy()
+        kwargs = generated_attributes | kwargs
+        return cls(**kwargs)
