52745df8729d818c389d6adfc0bd3fce3957ee5a
Ivan Krasnoshchok <ivankras67@gmail.com>
2023-05-13 22:13:49 +0300

    Add new abstract repositories
    Add AbstractFeedForwardNetworkRepository
    Add AbstractTrainingSessionRepository

---
+++ model_service/domain/repositories/abstract_feed_forward_network_repository.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+from model_service.domain.entities.neural_network.feed_forward_network import FeedForwardNetwork
+
+
+class AbstractFeedForwardNetworkRepository(ABC):
+    @abstractmethod
+    def save(self, feed_forward_network: FeedForwardNetwork) -> FeedForwardNetwork:
+        """Save feed forward network info to database"""
+
+    @abstractmethod
+    def get_by_id(self, feed_forward_network_id: str) -> FeedForwardNetwork | None:
+        """Get feed forward network info by id"""
+
+    @abstractmethod
+    def get_user_networks(self, user: str) -> list[FeedForwardNetwork]:
+        """Get list of user's feed forward networks"""
---
+++ model_service/domain/repositories/abstract_training_session_repository.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+from model_service.domain.entities.training.training_session import TrainingSession
+
+
+class AbstractTrainingSessionRepository(ABC):
+    @abstractmethod
+    def save(self, training_session: TrainingSession) -> TrainingSession:
+        """Save training session info to database"""
+
+    @abstractmethod
+    def get_by_id(self, training_session_id: str) -> TrainingSession | None:
+        """Get training session info by id"""
+
+    @abstractmethod
+    def get_user_sessions(self, user: str) -> list[TrainingSession]:
+        """Get list of user's training sessions"""
